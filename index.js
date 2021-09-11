23f4357ef6196c5f940ee6ffee9f14fe3219c5f6
mckim <kmc04@naver.com>
2021-09-12 03:08:56 +0900

    mongodb 연결하기

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+main()
+.then(() => console.log('DB 연결 성공'))
+.catch(err => console.log(err))
+
+async function main() {
+    await mongoose.connect(process.env.MONGO_URI)
+}
 app.listen(8800, () => {
     console.log('서버 오픈')
 })
