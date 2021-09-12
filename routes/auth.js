b7ec81f0f269ae052d19a5335f33904501ea466f
mckim <kmc04@naver.com>
2021-09-12 16:38:06 +0900

    crypto.js를 사용한 패스워드 암호화

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 const User = require('../models/User');
 
+const cryptoJS = require("crypto-js");
+
 // 회원가입
 router.post("/register", async (req,res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: cryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString(),
     });
     
 
