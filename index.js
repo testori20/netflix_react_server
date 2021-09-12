7b3c79457a8d5e4dba1a6e76db95a9b8de4f023e
mckim <kmc04@naver.com>
2021-09-12 16:32:08 +0900

    회원가입 기능 라우터 추가

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+
+// 환경변수
 const dotenv = require('dotenv');
 
-dotenv.config();
+// 라우터
+const authRoute = require('./routes/auth');
 
-main()
-.then(() => console.log('DB 연결 성공'))
-.catch(err => console.log(err))
+dotenv.config();
 
 async function main() {
     await mongoose.connect(process.env.MONGO_URI)
 }
+
+// 1. json 받기 설정
+app.use(express.json());
+
+// 2. mongodb 연결
+main()
+.then(() => console.log('DB 연결 성공'))
+.catch(err => console.log(err))
+
+// 3. 라우터 분리 (미들웨어 추가)
+// /api/auth 에 대한 요청을 할때 authRoute에서 작성한 코드대로 작동하게 된다
+app.use("/api/auth", authRoute);
+
+// 4. 서버 오픈
 app.listen(8800, () => {
     console.log('서버 오픈')
 })
--- models/User.js
+++ models/User.js
@@ -13,7 +13,7 @@ const UserSchema = new mongoose.Schema(
             unique: true,
         },
         password: {
-            type: String,
+            type: String, 
             required: true,
         },
         profilePic: {
