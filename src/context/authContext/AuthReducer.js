a92fabed2c47c288c0fb7676562e6d9271ff784e
mckim <kmc04@naver.com>
2021-09-16 02:13:12 +0900

    context-api 적용

---
+++ src/context/authContext/AuthReducer.js
@@ -0,0 +1,29 @@
+
+// Action을 받아서, 그를 통해 context 상태를 업데이트
+
+const AuthReducer = (state, action) => {
+    switch(action.type) {
+        case "LOGIN_START":
+            return {
+                user: null,
+                isFetching: true,
+                error: false,
+            }
+        case "LOGIN_SUCCESS":
+            return {
+                user: action.payload,
+                isFetching: false,
+                error: false,
+            }
+        case "LOGIN_FAILURE":
+            return {
+                user: null,
+                isFetching: false,
+                error: true,
+            }
+        default:
+            return {...state}
+    }
+};
+
+export default AuthReducer;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { AuthContextProvider } from './context/authContext/AuthContext';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+      {/* 모든 component에서 접근할 수 있는 값이 생겼다 */}
+        <AuthContextProvider>
+            <App />
+        </AuthContextProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
+)
